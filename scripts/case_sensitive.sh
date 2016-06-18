3915fffe8063a1aede92ab5bf1e093885013ffe3
Mathias Fredriksson <mafredri@gmail.com>
2016-06-19 02:44:02 +0300

    Allow case sensitive builds of ncurses

---
+++ scripts/case_sensitive.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+emulate -L zsh
+
+cs_create() {
+	local image=$1
+	local name=$2
+	local size=${3:-250m}
+	[[ -f $image ]] && return
+	hdiutil create -type SPARSE -fs 'Case-sensitive Journaled HFS+' -size $size -volname $name $image
+}
+
+cs_attach() {
+	local image=$1
+	local target=$2
+	hdiutil attach -mountpoint $target $image
+}
+
+cs_detach() {
+	local target=$1
+	hdiutil detach $target
+}
+
+cs_compact() {
+	local image=$1
+	hdiutil compact $image -batteryallowed
+}
