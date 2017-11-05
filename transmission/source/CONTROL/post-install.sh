d23598b76d5610bb13f892b54e9fd1453a393868
Mathias Fredriksson <mafredri@gmail.com>
2017-11-05 15:38:01 +0200

    transmission: Use tab indentation in all scripts

--- transmission/source/CONTROL/post-install.sh
+++ transmission/source/CONTROL/post-install.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 if [[ -z $APKG_PKG_DIR ]]; then
-    PKG_DIR=/usr/local/AppCentral/transmission
+	PKG_DIR=/usr/local/AppCentral/transmission
 else
-    PKG_DIR="${APKG_PKG_DIR}"
+	PKG_DIR="${APKG_PKG_DIR}"
 fi
 
 CONFIG="${PKG_DIR}/config"
 
 case "${APKG_PKG_STATUS}" in
-    install)
-        ;;
-    upgrade)
-        # Restore configuration
+	install)
+		;;
+	upgrade)
+		# Restore configuration
 		if [[ ! -d $CONFIG ]]; then
 			mkdir "${CONFIG}"
 		fi
-        cp -af "${APKG_TEMP_DIR}"/* "${CONFIG}/"
-        ;;
-    *)
-        ;;
+		cp -af "${APKG_TEMP_DIR}"/* "${CONFIG}/"
+		;;
+	*)
+		;;
 esac
 
 chown -R admin:administrators "${CONFIG}"
